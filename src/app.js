40ff0206c1d56aa9c8e3c960dedb9c1fe0db80b5
KARAN1234RAJ <36782443+KARAN1234RAJ@users.noreply.github.com>
2024-03-27 19:56:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
 const express = require("express");
+
 const dotenv = require("dotenv");
+
 const app = express();
+
 app.use(express.json());
+
 dotenv.config();
 
 // Write a GET route to return the value stored in the NUMBER variable stored in the .env file
+
 /*
+
 {
-    "number" : "2"
+
+"number" : "2"
+
 }
+
 */
+
 const data = process.env.NUMBER;
-console.log("data==",data.length);
+
 app.get("/api/get-env", (req, res) => {
-  //Write your code here
-  if (data.length>1) {
+  // Check if data is not undefined or null
+
+  if (data !== undefined && data !== null) {
     res.status(200).json({
       status: "success",
+
       number: data,
     });
   } else {
